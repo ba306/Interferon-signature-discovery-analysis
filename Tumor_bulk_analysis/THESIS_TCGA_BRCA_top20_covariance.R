a90a0e205e547ef5283b3bad9c6ac4e7123161a9
ba306 <bogacaybey@gmail.com>
2025-05-26 23:59:38 +0200

    BRCA analysis cleaned, updated, better resolution

--- Tumor_bulk_analysis/THESIS_TCGA_BRCA_top20_covariance.R
+++ Tumor_bulk_analysis/THESIS_TCGA_BRCA_top20_covariance.R
@@ -1,11 +1,12 @@
-library(xopdata)
+library(xopdata) # internal package
 library(dplyr)
 library(foreach)
 library(reshape2)
 library(readxl)
 library("ComplexHeatmap")
 library(gridtext)
-library(ProfilerAPI2)
+library(ProfilerAPI2) # internal package
+source("functions/RosettaSX_heatmap.R")
 
 # Comparing our IFN signatures against other signatures in a similar analysis done by 
 # Kreis et al. in RosettaSX analysis on TCGA BRCA
@@ -53,6 +54,8 @@ crc_tmm_gexp = merck_tcga_rna_wtstmm_gexp() %>%
                 quant_tmm_tpm) %>%
   dplyr::collect()
 
+crc_tmm_gexp <- saveRDS(crc_tmm_gexp,"data/Tumor_bulk/crc_tmm_gexp.RDS")
+crc_tmm_gexp <- readRDS("data/Tumor_bulk/crc_tmm_gexp.RDS")
 
 #### Coherence score ####
 # also calculate fraction of genes found/ whole signature
@@ -115,6 +118,9 @@ gset = merck_tcga_rna_wts_gset() %>%
                 quant_signature_score, quant_coherence_score) %>%
   dplyr::collect() 
 
+gset <- saveRDS(gset,"data/Tumor_bulk/gset.RDS")
+gset <- readRDS("data/Tumor_bulk/gset.RDS")
+
 gset$id_geneset_name=gsub("_\\d+$", "", gset$id_geneset_name)
 
 # Remove internal gene sets e-g- Eike internal
@@ -138,129 +144,6 @@ input_heat=
 sig_palette = circlize::colorRamp2(breaks = c(-2, -.5, 0, .5, 2), 
                                    colors = c("#6473ed", "#2fd0e0", "#e8cbae",
                                               "#e6b83c", "#bf3255"))
-
-# Function to create heatmap for a given signature
-# Choose top correlated or covariated signatures
-
-rosettasx_heatmap_func=function(data,
-                                covariance=F,
-                                correlation=F,
-                                selected_signature=NULL,
-                                top=20, # top 20 correlated or covariance 
-                                textsize=8, # size for top signature labels
-                                # titlesize=8, 
-                                label_size=12, # size for selected signature labels
-                                signature_colors=NULL,# signature score colors
-                                row_annot_colors=NULL # covariance or correlation color scale
-){
-  
-  
-  # order samples by selected signature
-  col_order=order(data[,selected_signature])
-  
-  # Create column annotations for main heatmap
-  exp_selected=data[,selected_signature,drop=F]
-  colnames(exp_selected)="exp"
-  
-  col_anno=ComplexHeatmap::HeatmapAnnotation(
-    df                   = exp_selected,
-    show_legend          = F,
-    na_col               = "grey",
-    show_annotation_name = TRUE,
-    col=list(exp=signature_colors),
-    simple_anno_size_adjust  = TRUE,
-    annotation_label = selected_signature)
-  
-  
-  # Either correlation or covariance based column annotation
-  
-  if(covariance==T){
-    # Covariance between selected signature and other signatures 
-    cov_Df=data %>%
-      cov() %>%
-      .[, grepl(selected_signature, colnames(data))] %>%
-      as.matrix()
-    
-    cov_Df=cov_Df[!grepl(selected_signature,rownames(cov_Df)),,drop=F]
-    
-    colnames(cov_Df)="Covariance"
-    
-    top_signatures=order(cov_Df[,1],decreasing = T)[1:top]
-    
-    row_anno=ComplexHeatmap::HeatmapAnnotation(
-      df                   = cov_Df[top_signatures,,drop=F],
-      show_legend          = T,
-      na_col               = "grey",
-      show_annotation_name = TRUE,
-      col=list(Covariance=row_annot_colors),
-      simple_anno_size_adjust  = TRUE,
-      which="row",
-      annotation_legend_param  = base::list(
-        direction      = "horizontal",
-        # title_position = "topleft",
-        title_gp = gpar(fontsize = label_size, fontface = "bold"),
-        labels_gp = gpar(fontsize = label_size)
-        
-      )
-    )
-    
-  }else if(correlation==T){
-    # Correlation between selected signature and other signatures 
-    cor_Df=data %>%
-      cor() %>%
-      .[, grepl(selected_signature, colnames(data))] %>%
-      as.matrix()
-    
-    cor_Df=cor_Df[!grepl(selected_signature,rownames(cor_Df)),,drop=F]
-    
-    colnames(cor_Df)="Correlation"
-    
-    top_signatures=order(cor_Df[,1],decreasing = T)[1:top]
-    
-    row_anno=ComplexHeatmap::HeatmapAnnotation(
-      df                   = cor_Df[top_signatures,,drop=F],
-      show_legend          = T,
-      na_col               = "grey",
-      show_annotation_name = TRUE,
-      col=list(Correlation=row_annot_colors),
-      simple_anno_size_adjust  = TRUE,
-      which="row",
-      annotation_legend_param  = base::list(
-        direction      = "horizontal",
-        # title_position = "topleft",
-        title_gp = gpar(fontsize = label_size, fontface = "bold"),
-        labels_gp = gpar(fontsize = label_size)
-        
-      )
-    )
-    
-  }
-  
-  # Plotting
-  # Main heatmap
-  
-  data_heatmap=data[,!grepl(selected_signature,colnames(data))]
-  
-  main_ht=Heatmap(t(data_heatmap[,  top_signatures]),show_column_names = F,cluster_rows = F,
-                  cluster_columns = F,
-                  column_order = col_order,
-                  top_annotation       = col_anno,
-                  left_annotation  = row_anno,
-                  col                  = signature_colors,
-                  heatmap_legend_param = base::list(
-                    direction      = "horizontal",
-                    # title_position = "topleft",
-                    title_gp = gpar(fontsize = label_size, fontface = "bold"),
-                    labels_gp = gpar(fontsize = label_size)
-                    
-                  ),name="Signature score",row_names_gp = gpar(fontsize = textsize)
-                  
-  )
-  draw(main_ht,heatmap_legend_side="top",
-       annotation_legend_side      = "bottom"
-  )
-  
-}
 
 
 #### Plotting ####
